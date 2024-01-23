71febad765d539df00b6abdb4a719814dd71148d
thinline20 <pekochan069@gmail.com>
2024-01-24 02:01:22 +0900

    Add youtube timeline tool icon

--- src/components/icons/youtube/Timeline.tsx
+++ src/components/icons/youtube/Timeline.tsx
@@ -1,3 +1,11 @@
+import { AiOutlineYoutube } from "solid-icons/ai";
+import { TbTimelineEventText } from "solid-icons/tb";
+
 export const TimelineIcon = () => {
-	return <div>youtube timeline</div>;
+	return (
+		<div class="flex gap-2">
+			<AiOutlineYoutube class="w-11 h-11 md:w-14 md:h-14" />
+			<TbTimelineEventText class="w-11 h-11 md:w-14 md:h-14" />
+		</div>
+	);
 };
