c951748af62ae65db05c84d701e868f82b05415b
thinline20 <pekochan069@gmail.com>
2024-01-24 21:44:17 +0900

    Replace Input with kobalte TextField

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,69 @@
-import type { Component, ComponentProps } from "solid-js"
-import { splitProps } from "solid-js"
+import { TextField } from "@kobalte/core";
+import type { Component, ComponentProps, JSX } from "solid-js";
+import { Show, splitProps } from "solid-js";
 
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 
-const Input: Component<ComponentProps<"input">> = (props) => {
-  const [, rest] = splitProps(props, ["type", "class"])
-  return (
-    <input
-      type={props.type}
-      class={cn(
-        "border-input ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border bg-transparent px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-        props.class
-      )}
-      {...rest}
-    />
-  )
-}
+type InputProps = ComponentProps<"input"> & {
+	value?: string;
+	defaultValue?: string;
+	onChange?: (value: string) => void;
+	name?: string;
+	validationState?: "valid" | "invalid";
+	required?: boolean;
+	disabled?: boolean;
+	readOnly?: boolean;
+	children?: JSX.Element;
+	labelClass?: string;
+};
 
-export { Input }
+const Input: Component<InputProps> = (props) => {
+	const [, rest] = splitProps(props, [
+		"value",
+		"defaultValue",
+		"onChange",
+		"name",
+		"validationState",
+		"required",
+		"disabled",
+		"readOnly",
+		"type",
+		"class",
+		"children",
+		"labelClass",
+	]);
+
+	return (
+		<TextField.Root
+			value={props.value}
+			defaultValue={props.defaultValue}
+			onChange={props.onChange}
+			name={props.name}
+			validationState={props.validationState}
+			required={props.required}
+			disabled={props.disabled}
+			readOnly={props.readOnly}
+		>
+			<Show when={props.children}>
+				<TextField.Label
+					class={cn(
+						"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+						props.labelClass,
+					)}
+				>
+					{props.children}
+				</TextField.Label>
+			</Show>
+			<TextField.Input
+				type={props.type}
+				class={cn(
+					"border-input ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border bg-transparent px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+					props.class,
+				)}
+				{...rest}
+			/>
+		</TextField.Root>
+	);
+};
+
+export { Input };
