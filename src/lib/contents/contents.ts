682671b005da87f6a0ef7a6aee4313ca890e652f
thinline20 <pekochan069@gmail.com>
2024-02-01 22:42:26 +0900

    Remove ; in tldr name and use Maybe type

--- src/lib/contents/contents.ts
+++ src/lib/contents/contents.ts
@@ -1,30 +1,48 @@
+import { None, Some } from "~/lib/types";
 import type { Contents } from "./contents-types";
 import { tldr } from "./tldr";
 import { tools } from "./tools";
 
 export const contents: Contents[] = [
-	{
-		name: "Tools",
-		slug: "tools",
-		categories: tools,
-	},
-	{
-		name: "tl;dr",
-		slug: "tldr",
-		categories: tldr,
-	},
+  {
+    name: "Tools",
+    slug: "tools",
+    categories: tools,
+  },
+  {
+    name: "tldr",
+    slug: "tldr",
+    categories: tldr,
+  },
 ];
 
-export function getContent(largeCategorySlug: string, categorySlug: string, contentSlug: string) {
-	const largeCategory = contents.find((c) => c.slug === largeCategorySlug);
-	if (!largeCategory) {
-		return null;
-	}
+export function getContent(
+  largeCategorySlug: string,
+  categorySlug: string,
+  contentSlug: string,
+) {
+  const largeCategory = contents.find((c) => c.slug === largeCategorySlug);
+  if (!largeCategory) {
+    return None();
+  }
 
-	const category = largeCategory.categories.find((c) => c.slug === categorySlug);
-	if (!category) {
-		return null;
-	}
+  const category = largeCategory.categories.find(
+    (c) => c.slug === categorySlug,
+  );
+  if (!category) {
+    return None();
+  }
 
-	return category.contents.find((c) => c.slug === contentSlug);
+  const content = category.contents.find((c) => c.slug === contentSlug);
+
+  if (!content) {
+    return None();
+  }
+
+  return Some({
+    largeCategory: largeCategory.name,
+    category: category.name,
+    name: content.name,
+    description: content.description,
+  });
 }
