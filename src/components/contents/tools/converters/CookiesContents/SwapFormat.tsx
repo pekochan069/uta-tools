d8c3b123eb8b7bd3c602b20e0ec07edcdcfc7243
thinline20 <pekochan069@gmail.com>
2024-02-07 00:53:52 +0900

    Add cookie format swap button

---
+++ src/components/contents/tools/converters/CookiesContents/SwapFormat.tsx
@@ -0,0 +1,29 @@
+import { IoSwapVertical } from "solid-icons/io";
+
+import { Button } from "~/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+import { format } from "./atoms";
+
+export const SwapFormat = () => {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => {
+            const currentFormat = format.get();
+            format.set({ from: currentFormat.to, to: currentFormat.from });
+          }}
+        >
+          <IoSwapVertical class="h-[1.2rem] w-[1.2rem]" />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>Swap format</TooltipContent>
+    </Tooltip>
+  );
+};
