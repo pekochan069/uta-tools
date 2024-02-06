e52ec1360f2e4986011bef030cd5efbcf0ede8c1
thinline20 <pekochan069@gmail.com>
2024-02-07 00:53:29 +0900

    Add cookie icon

--- src/components/icons/tools/converters/Cookies.tsx
+++ src/components/icons/tools/converters/Cookies.tsx
@@ -1,3 +1,7 @@
+import { TbCookie } from 'solid-icons/tb'
+
 export const CookiesIcon = () => {
-  return <div>Cookies</div>;
+  return <div>
+    <TbCookie class="h-14 w-14 @[148px]:h-[4.5rem] @[148px]:w-[4.5rem] @[200px]:h-[5.5rem] @[200px]:w-[5.5rem]" />
+  </div>;
 };
