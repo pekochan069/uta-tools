22412603264aa3cdc4556be885e90228269c4050
pekochan069 <pekochan069@gmail.com>
2024-03-14 23:25:46 +0900

    Only apply transitions to transform

--- src/components/navbar/ThemeToggle.tsx
+++ src/components/navbar/ThemeToggle.tsx
@@ -38,8 +38,8 @@ export default () => {
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <As component={Button} variant="ghost" size="icon">
-              <TbSun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <TbMoon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <TbSun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0" />
+              <TbMoon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
               <span class="sr-only">Toggle theme</span>
             </As>
           </DropdownMenuTrigger>
