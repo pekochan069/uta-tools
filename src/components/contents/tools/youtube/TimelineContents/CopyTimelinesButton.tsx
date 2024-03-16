63334ffa335187b158af09591f0a8df084e229cd
pekochan069 <pekochan069@gmail.com>
2024-03-16 22:18:13 +0900

    Fix tooltip placement

--- src/components/contents/tools/youtube/TimelineContents/CopyTimelinesButton.tsx
+++ src/components/contents/tools/youtube/TimelineContents/CopyTimelinesButton.tsx
@@ -22,7 +22,7 @@ export default () => {
       >
         Copy
       </Button>
-      <div class="absolute top-12 z-50 w-max -translate-x-1/4 rounded-md border border-border bg-background px-3 py-1.5 text-center text-sm opacity-0 shadow-md transition-opacity duration-200 ease-in-out group-hover:opacity-100">
+      <div class="absolute right-0 xl:translate-x-1/4 top-12 z-50 w-max  rounded-md border border-border bg-background px-3 py-1.5 text-center text-sm opacity-0 shadow-md transition-opacity duration-200 ease-in-out group-hover:opacity-100">
         Copy all timeline
       </div>
     </div>
