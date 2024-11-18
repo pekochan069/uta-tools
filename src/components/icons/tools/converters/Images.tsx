f23a553d501c14e7618b142a0036dda31ce7cafe
thinline20 <pekochan069@gmail.com>
2024-11-19 01:40:38 +0900

    Update image converter icon

--- src/components/icons/tools/converters/Images.tsx
+++ src/components/icons/tools/converters/Images.tsx
@@ -1,3 +1,11 @@
+import { TbArrowDown } from "solid-icons/tb";
+
 export function ImagesIcon() {
-  return <div>Icon</div>;
+  return (
+    <div class="flex flex-col items-center gap-1 text-xl font-bold">
+      <span>PNG</span>
+      <TbArrowDown class="size-6" />
+      <span>WebP</span>
+    </div>
+  );
 }
