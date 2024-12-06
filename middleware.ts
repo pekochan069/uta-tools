67e329f4366a4e7fa56eb3a0398297b00276b6ec
thinline20 <pekochan069@gmail.com>
2024-12-07 03:56:15 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,12 @@ import { next, rewrite } from "@vercel/edge";
 function middleware(request: Request) {
   const url = new URL(request.url);
 
-  if (!url.pathname.startsWith("/ko") && !url.pathname.startsWith("/en")) {
+  const paths = url.pathname.split("/");
+  if (paths[1] !== "en" && paths[1] !== "ko") {
     return rewrite(new URL(`/ko${url.pathname}`, request.url));
   }
 
-  if (url.pathname.endsWith("/tools/converters/images")) {
+  if (url.pathname.endsWith("/converters/images")) {
     return next({
       headers: {
         "Cross-Origin-Embedder-Policy": "require-corp",
